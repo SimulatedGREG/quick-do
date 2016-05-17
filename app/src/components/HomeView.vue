46dab79f886fa86296411f1b9af095fc4b7b6225
SimulatedGREG <simulatedgreg@gmail.com>
2016-05-17 16:56:37 -0500

    nice scrollbar

--- app/src/components/HomeView.vue
+++ app/src/components/HomeView.vue
@@ -1,4 +1,4 @@
-<style>
+<style lang="scss">
   @font-face {
     font-family: 'Roboto';
     src: url(./HomeView/assets/roboto.ttf);
@@ -10,12 +10,45 @@
     background-color: #ecf0f1;
     font-family: 'Roboto', sans-serif;
   }
+
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+  }
+
+  main {
+    position: absolute;
+    top: 86px;
+    width: 100%;
+  }
+
+  ::-webkit-scrollbar-track {
+  	background-color: rgba(#1abc9c, .1);
+    display: none;
+  }
+
+  ::-webkit-scrollbar {
+  	width: 6px;
+  	background-color: rgba(#1abc9c, .1);
+    display: none;
+  }
+
+  ::-webkit-scrollbar-thumb {
+  	background-color: rgba(#16a085, .5);
+    display: none;
+  }
 </style>
 
 <template>
-  <top></top>
-  <controls :selected="selected"></controls>
-  <list :selected="selected"></list>
+  <header>
+    <top></top>
+    <controls :selected="selected"></controls>
+  </header>
+  <main>
+    <list :selected="selected"></list>
+  </main>
 </template>
 
 <script>
--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ let config = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         query: {
-          limit: 10000,
+          limit: 0,
           name: 'fonts/[name].[hash:7].[ext]'
         }
       }
